eb675d4ba8a72e0e385824bfb1b547574356a67f
TeimurazTs <temotsuladze99@gmail.com>
2023-05-07 19:19:10 +0400

    last changes

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Route, Params } from '@angular/router';
+import { ClipService } from 'src/app/services/clip.service';
+import IClip from 'src/app/models/clip.model';
 
 @Component({
   selector: 'app-manage',
@@ -8,8 +10,13 @@ import { Router, ActivatedRoute, Route, Params } from '@angular/router';
 })
 export class ManageComponent implements OnInit {
   videoOrder = '1';
+  clips: IClip[] = [];
 
-  constructor(private router: Router, private route: ActivatedRoute) {}
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private clipService: ClipService
+  ) { }
 
   sort(event: Event) {
     const { value } = event.target as HTMLSelectElement;
@@ -25,6 +32,16 @@ export class ManageComponent implements OnInit {
   ngOnInit(): void {
     this.route.queryParams.subscribe((params: Params) => {
       this.videoOrder = params['sort'] === '2' ? params['sort'] : '1';
+    });
+    this.clipService.getUserClips().subscribe(docs => {
+      this.clips = [];
+
+      docs.forEach(doc => {
+        this.clips.push({
+          docID: doc.id,
+          ...doc.data()
+        })
+      })
     });
   }
 }
--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -9,6 +9,7 @@ import { last, switchMap } from 'rxjs/operators';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import firebase from 'firebase/compat/app';
 import { ClipService } from 'src/app/services/clip.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-upload',
@@ -19,7 +20,8 @@ export class UploadComponent implements OnDestroy {
   constructor(
     private storage: AngularFireStorage,
     private auth: AngularFireAuth,
-    private clipsService: ClipService
+    private clipsService: ClipService,
+    private router: Router
   ) {
     auth.user.subscribe((user) => (this.user = user));
   }
@@ -85,21 +87,28 @@ export class UploadComponent implements OnDestroy {
         switchMap(() => clipRef.getDownloadURL())
       )
       .subscribe({
-        next: (url) => {
+        next: async (url) => {
           const clip = {
             uid: this.user?.uid as string,
             displayName: this.user?.displayName as string,
             title: this.title.value,
             fileName: `${clipFileName}.mp4`,
             url,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp()
           };
 
-          this.clipsService.createClip(clip);
+          const clipDocRef = await this.clipsService.createClip(clip);
 
           this.alertColor = 'green';
           this.alertMsg =
             'Success! Your clip is now ready to share with the world';
           this.showPercentage = false;
+
+          setTimeout(() => {
+            this.router.navigate([
+              'clip', clipDocRef.id
+            ])
+          })
         },
         error: (error) => {
           this.uploadForm.enable();
@@ -107,7 +116,6 @@ export class UploadComponent implements OnDestroy {
           this.alertMsg = 'Upload failed! Please try again later.';
           this.inSubmission = true;
           this.showPercentage = false;
-          console.log(error);
         },
       });
   }
